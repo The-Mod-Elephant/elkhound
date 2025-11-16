3cb14039f9820e7af77c101ad9962128652974dd
dark0dave <dark0dave@mykolab.com>
2025-11-16 15:43:15 +0000

    feat(nix): Add nix flake
    Signed-off-by: dark0dave <dark0dave@mykolab.com>

---
+++ flake.nix
@@ -0,0 +1,74 @@
+{
+  description = "elkhound";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/2cd3cac16691a933e94276f0a810453f17775c28";
+  };
+  outputs = { self, nixpkgs }:
+    let systems = [
+      "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"
+    ];
+    forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+    in {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in {
+          default = pkgs.mkShell {
+            name = "elkhound-shell";
+            src = self;
+            # Libs
+            buildInputs = with pkgs; [
+              bison
+              cmake
+              flex
+              gnumake
+            ];
+            # Tools
+            nativeBuildInputs = with pkgs; [
+              direnv
+              git
+            ];
+            # Env
+            shellHook = ''
+            '';
+          };
+        });
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in pkgs.stdenv.mkDerivation {
+          pname = "elkhound";
+          version = "1.0";
+
+          src = ./src;
+
+          nativeBuildInputs = with pkgs; [
+            bison
+            cmake
+            flex
+            gnumake
+          ];
+
+          buildInputs = with pkgs; [
+            git
+          ];
+
+          configurePhase = ''
+            cmake -Wno-dev -DEXTRAS=OFF -DOCAML=OFF -B $out/build -D CMAKE_BUILD_TYPE=Release
+          '';
+
+          buildPhase = ''
+            make -j$(nproc) -C $out/build
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp --preserve=mode -r $out/build/elkhound/elkhound $out/bin/elkhound
+            rm -rf $out/build
+          '';
+          enableParallelBuilding = true;
+        }
+      );
+    };
+}
